66f42e14419fa88114ba35b21c4ec1faff097853
yasinxdxd <sesden19611@gmail.com>
2020-11-22 23:34:45 +0300

    Input file added, Mouse and Keyboard added

--- Hello_OpenGL/Hello_OpenGL/GO/Input/Mouse.cpp
+++ Hello_OpenGL/Hello_OpenGL/GO/Input/Mouse.cpp
@@ -5,11 +5,44 @@ namespace go
 
     Mouse::Mouse()
     {
-        
+        is_mouse_pressed = false;
+        m_button = Left;
     }
 
     Vec2d& Mouse::getPosition(void)
     {
         return m_position;
+    }
+
+    GObool Mouse::isMousePressed(go::Mouse::MouseButton button)
+    {
+        m_button = button;
+        return is_mouse_pressed;
+    }
+
+
+    //callbacks:
+    void Mouse::cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+    {
+
+    }
+
+    void Mouse::mouse_button_callback(GLFWwindow* window, GOint button, GOint action, GOint mods)
+    {
+        switch(m_button)
+        {
+        case Left:
+            if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+                Mouse::is_mouse_pressed = true;
+            else
+                Mouse::is_mouse_pressed = false;
+            break;
+        case Right:
+            if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+                Mouse::is_mouse_pressed = true;
+            else
+                Mouse::is_mouse_pressed = false;
+            break;
+        }
     }
 }
--- Hello_OpenGL/Hello_OpenGL/GO/Input/Mouse.h
+++ Hello_OpenGL/Hello_OpenGL/GO/Input/Mouse.h
@@ -3,7 +3,7 @@
 
 
 #include "Math/Vectors.h"
-
+#include "GLFW/glfw3.h"
 
 namespace go
 {
@@ -11,13 +11,34 @@ namespace go
 	class Mouse
 	{
 	public:
+		enum MouseButton
+		{ Left, Right };
+
+	public:
 		Mouse();
 		static Vec2d& getPosition(void);
+		static GObool isMousePressed(go::Mouse::MouseButton);
 	
+
+
+	private:
+		//returning values
+		static inline GObool is_mouse_pressed; //static GObool isMousePressed(go::Mouse::MouseButton);
+		static inline MouseButton m_button;
+
+
 	private:
 		static inline go::Vec2d m_position;
 
+	private:
+		//callbacks:
+		static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
+		static void mouse_button_callback(GLFWwindow* window, GOint button, GOint action, GOint mods);
+
+
+
 	friend class Window;
+	//friend class WindowEvent;
 	};
 
 }
