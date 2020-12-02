b2a8031dddd2ef2bf2b6d10b7e9ad66d62ee1794
yasinxdxd <sesden19611@gmail.com>
2020-12-02 13:49:47 +0300

    Keyboard class that used GLFW

---
+++ Hello_OpenGL/Hello_OpenGL/GO/Input/Keyboard.cpp
@@ -0,0 +1,24 @@
+#include "Keyboard.h"
+
+namespace go
+{
+	
+	
+	
+	GObool Keyboard::isKeyPressed(go::Keyboard::Key key)
+	{
+		m_key = key;
+		return is_key_pressed;
+	}
+	
+
+	//callbacks:
+	void Keyboard::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		if (key == m_key && action == GLFW_PRESS)
+			is_key_pressed = true;
+		else
+			is_key_pressed = false;
+	}
+
+}
