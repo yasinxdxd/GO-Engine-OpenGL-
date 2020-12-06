b04f02c9dd465604c0d998f65fb6b5a1871b0420
yasinxdxd <sesden19611@gmail.com>
2020-12-06 21:52:54 +0300

    Aplication Abstract class added.

---
+++ Hello_OpenGL/Hello_OpenGL/GO/Core/Application.cpp
@@ -0,0 +1,29 @@
+#include "Application.h"
+
+namespace go
+{
+
+	/*
+
+	void Application::GO()
+	{
+		Application::Start();
+		
+		while (!window->isClose())
+		{
+			window->pollEvent();
+			
+			Application::Update();
+
+			window->clear();
+			Application::Draw();
+			window->display();
+		}
+
+		Application::Destroy();
+	}
+	*/
+
+
+
+}
